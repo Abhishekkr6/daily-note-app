d9f3875f65d561b77baed84f2155e9a247cf0d54
Abhishek Kumar Tiwari <tiwariabhi62065@gmail.com>
2025-10-12 14:17:22 +0530

    feat: add csrf-token to allowed headers in CORS middleware

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -6,7 +6,7 @@ export function corsMiddleware(req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", origin);
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization,csrf-token");
   }
   if (req.method === "OPTIONS") {
     res.status(200).end();
